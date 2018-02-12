bcc359003605158135af41f741182490c3c6e162
DoubleVV <birjam.walid@gmail.com>
2018-02-12 16:39:21 +0100

    J'aime les moules a gauffres

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by DoubleVV on 12/02/2018.
+//
+
+#include <iostream>
+
+int main(){
+
+    std::cout << "Hey salut les amis c'est David lafarge pokemon" << std::endl;
+
+    return 0;
+}
